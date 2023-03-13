825c8999b1646eafdf395774c96374721926d269
Louise Reña <realouise175@gmail.com>
2023-03-13 16:02:04 +0800

    Updates

--- script.js
+++ script.js
@@ -5,3 +5,38 @@ dropDown.forEach((dropDowns) => {
         dropDowns.querySelector("i").classList.toggle("open");
     })
 })
+
+const toggleMenu = document.querySelector(".toggle__menu");
+const headerNav = document.querySelector(".header__content");
+const kamote = document.querySelector(".kamote")
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  headerNav.classList.toggle("open");
+  kamote.classList.toggle("overlay");
+})
+
+const starNav = document.querySelectorAll(".star__nav__link");
+const starContent = document.querySelectorAll('.coffee-tab__content');
+
+starNav.forEach((star) => {
+  star.addEventListener("click", () => {
+    removeActiveStar();
+    star.classList.add("active");
+    const activeContent= document.querySelector(`#${star.id}-content`);
+    removeActiveContent();
+    activeContent.classList.add("active");
+  })
+})
+
+function removeActiveStar (){
+  starNav.forEach((star) => {
+    star.classList.remove("active");
+  })
+}
+
+function removeActiveContent (){
+  starContent.forEach((star) => {
+    star.classList.remove("active");
+  })
+}
+
